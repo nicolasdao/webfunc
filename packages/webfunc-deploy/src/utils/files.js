cb889a1966bd2988e5b4b483d0a1e771ede5434e
Nicolas Dao <nicolas.dao@gmail.com>
2018-08-28 10:30:03 +1000

    feat: Add zipping nodejs.

--- packages/webfunc-deploy/src/utils/files.js
+++ packages/webfunc-deploy/src/utils/files.js
@@ -9,15 +9,16 @@
 const fs = require('fs-extra')
 const { join } = require('path')
 const { homedir } = require('os')
-const { error, exec, debugInfo, cmd } = require('./console')
 const _glob = require('glob')
+const archiver = require('archiver')
+const { toBuffer } = require('convert-stream')
+const { error, exec, debugInfo, cmd } = require('./console')
 
 const TEMP_FOLDER = join(homedir(), 'temp/webfunc')
 
 const createTempFolder = () => fs.ensureDir(TEMP_FOLDER)
 	.catch(() => {
 		console.log(error(`Failed to create temporary folder ${TEMP_FOLDER}.`))
-		process.exit(1)
 	})
 
 const glob = (pattern, options) => new Promise((success, failure) => _glob(pattern, options, (err, files) => {
@@ -27,34 +28,38 @@ const glob = (pattern, options) => new Promise((success, failure) => _glob(patte
 		success(files)
 }))
 
-const cloneProject = (src='', options={ debug:false }) => createTempFolder().then(() => {
+const cloneNodejsProject = (src='', options={ debug:false }) => createTempFolder().then(() => {
 	const { debug } = options || {}
 	const dst = join(TEMP_FOLDER, Date.now().toString())
 
-	if (debug)
-		console.log(debugInfo(`Copying content of folder '${src}' under temporary location '${dst}'...`))
+	if (debug) 
+		console.log(debugInfo(`Copying content of folder \n${src} \nto temporary location \n${dst}`))
 
 	return glob(join(src, '**/*.*'), { ignore: '**/node_modules/**' }).then(files => glob(join(src, '**/.*'), { ignore: '**/node_modules/**' }).then(dot_files => {
 		const all_files = [...(files || []), ...(dot_files || [])]
 		
 		if (debug)
-			console.log(debugInfo(`Found ${all_files.length} files under folder '${src}'. Copying them now...`))
+			console.log(debugInfo(`Found ${all_files.length} files under folder \n${src}\nCopying them now...`))
 
 		return Promise.all(all_files.map(f => {
 			return fs.copy(f, join(dst, f.replace(src, '')))
 				.then(() => null)
-				.catch(e => e)
+				.catch(e => {
+					console.log(error(`Failed to clone nodejs project located under \n${src}\nto the temporary location \n${dst}\nThis procedure is usually required to zip the project before uploading it to the selected provider.`))
+					throw e
+				})
 		})).then(values => {
 			const errors = values.filter(v => v != null)
 			if (errors.length > 0) {
-				console.log(error(`Could not copy the following files to ${TEMP_FOLDER}:`), '\n', errors)
-				process.exit(1)
+				console.log(error(`Could not copy the following files to ${TEMP_FOLDER}:`))
+				errors.forEach(err => console.log(error(err)))
+				throw new Error('Failed to copy some files.')
 			}
 
 			const npmCommand = `npm install --prefix ${dst}`
 
 			if (debug)
-				console.log(debugInfo(`Files successfully copied under '${dst}'. Executing command ${cmd(npmCommand)} now...`))
+				console.log(debugInfo(`Files successfully copied to \n${dst}\nExecuting command \n${cmd(npmCommand)}`))
 
 			return exec(npmCommand)
 				.then(() => {
@@ -64,6 +69,8 @@ const cloneProject = (src='', options={ debug:false }) => createTempFolder().the
 				.catch(() => {
 					if (debug)
 						console.log(debugInfo('Command failed.'))
+
+					throw new Error(`Command ${npmCommand} failed.`)
 				})
 				.then(() => dst)
 		})
@@ -74,7 +81,7 @@ const deleteFolder = (src, options={ debug:false }) => {
 	const { debug } = options || {}
 	
 	if (debug)
-		console.log(debugInfo(`Deleting folder '${src}'...`))
+		console.log(debugInfo(`Deleting folder ${src}`))
 
 	return fs.exists(src).then(result => {
 		if (result)
@@ -83,18 +90,77 @@ const deleteFolder = (src, options={ debug:false }) => {
 			return null
 	}).then(() => {
 		if (debug)
-			console.log(debugInfo(`Folder '${src}' successfully deleted.`))
+			console.log(debugInfo(`Folder \n${src} \nsuccessfully deleted.`))
 		return null
 	}).catch(e => {
 		if (debug) {
-			console.log(debugInfo(`Folder '${src}' could not be deleted.`))
+			console.log(debugInfo(`Folder \n${src} \ncould not be deleted.`))
 		}
 		return e
 	})
 }
 
-cloneProject(process.cwd(), { debug:true })
-	//.then(dst => dst ? deleteFolder(dst, { debug:true }) : null)
+const zipFolderToBuffer = (src, options={ debug:false }) => fs.exists(src).then(result => {
+	if (!result)
+		throw new Error(`Failed to zip folder ${src}. This folder does not exist.`)
+
+	const { debug } = options || {}
+
+	if (debug) 
+		console.log(debugInfo(`Starting to zip folder \n${src}.`))
+
+	const archive = archiver('zip')
+	const buffer = toBuffer(archive)
+
+	archive.on('warning', err => {
+		console.log(error('Warning while creating zip file'), err)
+	})
+
+	archive.on('error', err => {
+		throw err
+	})
+
+	archive.directory(src)
+	archive.finalize()
+	return buffer
+		.then(v => {
+			if (debug) 
+				console.log(debugInfo(`Folder \n${src} \nsuccessfully zipped to buffer.`))
+			return v
+		})
+		.catch(e => {
+			console.log(error(`Failed to zip folder ${src}`))
+			throw e
+		})
+})
+
+const zipNodejsProject = (src, options={ debug:false }) => {
+	const { debug } = options || {}
+
+	if (debug) 
+		console.log(debugInfo(`Starting to zip nodejs project located under\n${src}`))
+
+	return cloneNodejsProject(src, options)
+		.then(dst => zipFolderToBuffer(dst, options).then(buffer => ({ buffer, tempFolder: dst })))
+		.then(({ buffer, tempFolder }) => {
+			if (debug) {
+				const sizeMb = (buffer.length / 1024 / 1024).toFixed(2)
+				console.log(debugInfo(`The nodejs project located under\n${src}\nhas been successfully zipped to buffer (size: ${sizeMb}MB)`))
+			}
+
+			return deleteFolder(tempFolder, options)
+				.then(() => buffer)
+		})
+		.catch(e => {
+			console.log(error(`Failed to zip nodejs project located under\n${src}`))
+			throw e
+		})
+}
+
+zipNodejsProject(process.cwd(), { debug:true })
+
+//cloneNodejsProject(process.cwd(), { debug:true })
+//.then(dst => dst ? deleteFolder(dst, { debug:true }) : null)
 
 
 
